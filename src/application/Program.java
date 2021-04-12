9f3496ab5cdf3ecaadcb1f24954ea462c5df26cf
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-04-12 16:36:55 -0300

    Brasch master para main

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Ola mundo!");
-
+		System.out.println("Para mudar o padrão da branch do git");
+		System.out.println("basta escrever no terminal do git");
+		System.out.println("git checkout -b main");
 	}
 
 }

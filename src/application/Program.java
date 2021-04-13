196efdee19f0c65233052c7108f25f7981340bc7
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-04-13 12:00:43 -0300

    Tipos enumerado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,25 @@
 package application;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Ola mundo!");
-		System.out.println("Para mudar o padrão da branch do git");
-		System.out.println("basta escrever no terminal do git");
-		System.out.println("git checkout -b main");
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
 	}
+	
 
 }

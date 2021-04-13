eac46cdae33c1e3682d7f77215c4e6497e32bf0b
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-04-13 14:35:55 -0300

    Trabalhando com Composição

---
+++ src/entities/Department.java
@@ -0,0 +1,24 @@
+package entities;
+
+//classe departamento;
+public class Department {
+	private String name;
+	
+	public Department() {
+		//construtor vazio
+	}
+
+	public Department(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+	

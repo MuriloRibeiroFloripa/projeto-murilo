98ba2369e22964e395e91b31ea907d3a11b0d6d5
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-04-24 16:55:48 -0300

    Métodos abstratos

---
+++ src/metodos_abstratos_application/Program.java
@@ -0,0 +1,62 @@
+package metodos_abstratos_application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import metodos_abstratos_entities.Circle;
+import metodos_abstratos_entities.Rectangle;
+import metodos_abstratos_entities.Shape;
+import metodos_abstratos_entities.enums.Color;
+
+/*Métodos abstratos
+• São métodos que não possuem implementação.
+• Métodos precisam ser abstratos quando a classe
+é genérica demais para conter sua
+implementação.
+• Se uma classe possuir pelo menos um método
+abstrato, então esta classe também é abstrata.
+• Notação UML: itálico*/
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Shape> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of shapes: ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Shape #" + i + " data:");
+			System.out.print("Rectangle or Circle (r/c)? ");
+			char ch = sc.next().charAt(0);
+			System.out.print("Color (BLACK/BLUE/RED): ");
+			Color color = Color.valueOf(sc.next());
+			if (ch == 'r') {
+				System.out.print("Width: ");
+				double width = sc.nextDouble();
+				System.out.print("Height: ");
+				double height = sc.nextDouble();
+				list.add(new Rectangle(color, width, height));
+			}
+			else {
+				System.out.print("Radius: ");
+				double radius = sc.nextDouble();
+				list.add(new Circle(color, radius));
+			}
+		}
+		
+		System.out.println();
+		System.out.println("SHAPE AREAS:");
+		for (Shape shape : list) {
+			System.out.println(String.format("%.2f", shape.area()));
+		}
+		
+		sc.close();
+	}
+}

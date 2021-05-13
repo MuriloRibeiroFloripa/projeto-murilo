359213d96793f9ee5c3f2c2e1abf9e8047b9cbe8
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-05-12 22:40:37 -0300

    Como TreeSet compara os elementos

---
+++ src/generics/set_Interface/setTestaIgualdade/Program.java
@@ -0,0 +1,30 @@
+package generics.set_Interface.setTestaIgualdade;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* Como as coleções Hash testam igualdade?
+ * • Se hashCode e equals estiverem implementados:
+ * • Primeiro hashCode. Se der igual, usa equals para confirmar.
+ * • Lembre-se: String, Integer, Double, etc. já possuem equals e hashCode
+ * • Se hashCode e equals NÃO estiverem implementados:
+ * • Compara as referências (ponteiros) dos objetos.
+ * 
+ */
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Set<Product> set = new HashSet<>();
+		
+		set.add(new Product("TV", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
+		
+		//testando se o produto Notebook contem na lista
+		Product prod = new Product("Notebook", 1200.0);
+		System.out.println(set.contains(prod));		
+	}
+
+}

9a30d0b157b68729238f6677850093d8b95f7f1c
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-05-20 19:49:34 -0300

    Implemetação generica interface Funtion com map com metodos não estaticos

---
+++ src/generics/Interface/function/metodoNaoEstatico/application/Program.java
@@ -0,0 +1,25 @@
+package generics.Interface.function.metodoNaoEstatico.application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import generics.Interface.function.metodoNaoEstatico.entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		// map so funciona para stream não funciona para lista
+		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		
+		names.forEach(System.out::println);
+	}
+}
